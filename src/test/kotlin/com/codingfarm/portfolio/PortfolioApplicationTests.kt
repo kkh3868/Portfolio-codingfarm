07d3c3e40ade03193c2f714cdde20738e8c9b277
kkh3868 <rjsgml198@gmail.com>
2024-08-12 22:15:15 +0900

    Git과 Github

---
+++ src/test/kotlin/com/codingfarm/portfolio/PortfolioApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.codingfarm.portfolio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
