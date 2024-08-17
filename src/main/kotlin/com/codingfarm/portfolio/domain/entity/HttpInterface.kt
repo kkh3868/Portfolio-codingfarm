19c63847c68d3061563c7adefab08fdf7938033e
kkh3868 <rjsgml198@gmail.com>
2024-08-17 21:26:49 +0900

    엔티티 개발 - 연관관계 없음

--- src/main/kotlin/com/codingfarm/portfolio/domain/entity/HttpInterface.kt
+++ src/main/kotlin/com/codingfarm/portfolio/domain/entity/HttpInterface.kt
@@ -1,11 +1,29 @@
 package com.codingfarm.portfolio.domain.entity
 
 import jakarta.persistence.*
+import jakarta.servlet.http.HttpServletRequest
+import java.net.CacheRequest
 
 @Entity
-class HttpInterface : BaseEntity() {
+class HttpInterface(httpServeletRequest: HttpServletRequest) : BaseEntity() {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "http_interface_id")
     var id: Long? = null
+
+    var cookies: String? = httpServeletRequest.cookies
+        ?.map{ "${it.name}:${it.value}"}
+        ?.toString()
+
+    var referer: String? = httpServeletRequest.getHeader("referer")
+
+    var localAddr: String? = httpServeletRequest.localAddr
+
+    var remoteAddr: String? = httpServeletRequest.remoteAddr
+
+    var remoteHost: String? = httpServeletRequest.remoteHost
+
+    var requestURI: String? = httpServeletRequest.requestURI
+
+    var userAgent: String? = httpServeletRequest.getHeader("user-agent")
 }
--- src/main/kotlin/com/codingfarm/portfolio/domain/entity/Link.kt
+++ src/main/kotlin/com/codingfarm/portfolio/domain/entity/Link.kt
@@ -1,11 +1,22 @@
 package com.codingfarm.portfolio.domain.entity
 
 import jakarta.persistence.*
+import java.time.LocalDate
 
 @Entity
-class Link : BaseEntity() {
+class Link(
+    name: String,
+    content: String,
+    isActive: Boolean,
+) : BaseEntity() {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "link_id")
     var id: Long? = null
+
+    var name: String = name
+
+    var content: String = content
+
+    var isActive: Boolean = isActive
 }
