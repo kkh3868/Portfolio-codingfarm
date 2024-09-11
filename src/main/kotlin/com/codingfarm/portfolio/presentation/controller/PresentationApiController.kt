9442d05fa056d4eea5806016b3086be2ac576cc7
김건희 <rjsgml198@naver.com>
2024-09-11 17:17:45 +0900

    컨트롤러 개발

--- src/main/kotlin/com/codingfarm/portfolio/presentation/controller/PresentationApiController.kt
+++ src/main/kotlin/com/codingfarm/portfolio/presentation/controller/PresentationApiController.kt
@@ -1,14 +1,38 @@
 package com.codingfarm.portfolio.presentation.controller
 
+import com.codingfarm.portfolio.presentation.dto.IntroductionDTO
+import com.codingfarm.portfolio.presentation.dto.LinkDTO
+import com.codingfarm.portfolio.presentation.dto.ProjectDTO
+import com.codingfarm.portfolio.presentation.dto.ResumeDTO
+import com.codingfarm.portfolio.presentation.service.PresentationService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api")
-class PresentationApiController {
+class PresentationApiController(
+    private val presentationService: PresentationService
+) {
     @GetMapping("/test")
     fun test():String{
         return "OK"
+    }
+
+    @GetMapping("/v1/introductions")
+    fun getIntroductions():List<IntroductionDTO>{
+        return presentationService.getIntroductions()
+    }
+    @GetMapping("/v1/links")
+    fun getLinks():List<LinkDTO>{
+        return presentationService.getLinks()
+    }
+    @GetMapping("/v1/resume")
+    fun getResume():ResumeDTO{
+        return presentationService.getResume()
+    }
+    @GetMapping("/v1/projects")
+    fun getProjects():List<ProjectDTO>{
+        return presentationService.getProjects()
     }
 }
