37e89fe59fc1f70cd272b75354723c744ec655b4
kkh3868 <rjsgml198@gmail.com>
2024-09-20 13:51:12 +0900

    인터셉터 개발

--- src/main/kotlin/com/codingfarm/portfolio/presentation/interceptor/PresentationInterceptor.kt
+++ src/main/kotlin/com/codingfarm/portfolio/presentation/interceptor/PresentationInterceptor.kt
@@ -1,7 +1,24 @@
 package com.codingfarm.portfolio.presentation.interceptor
 
+import com.codingfarm.portfolio.domain.entity.HttpInterface
+import com.codingfarm.portfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+import java.lang.Exception
 
 @Component
-class PresentationInterceptor {
+class PresentationInterceptor(
+    private val httpInterfaceRepository: HttpInterfaceRepository
+) : HandlerInterceptor {
+    override fun afterCompletion(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+        ex: Exception?
+    ) {
+        val httpInterface = HttpInterface(request)
+        httpInterfaceRepository.save(httpInterface)
+    }
 }
