a64b086ff102e5e6bbf3e39918dacedab9962c5d
kkh3868 <rjsgml198@gmail.com>
2024-09-20 15:05:21 +0900

    공통 개발 - Exception과 Advice

--- src/main/kotlin/com/codingfarm/portfolio/admin/advice/AdminApiControllerAdvice.kt
+++ src/main/kotlin/com/codingfarm/portfolio/admin/advice/AdminApiControllerAdvice.kt
@@ -10,13 +10,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 @RestControllerAdvice
 class AdminApiControllerAdvice {
     val log = LoggerFactory.getLogger(AdminApiControllerAdvice::class.java)
+
     @ExceptionHandler
-    fun handlerException(e:AdminException):ResponseEntity<String>{
+    fun handlerException(e: AdminException): ResponseEntity<String> {
         log.info(e.message, e)
         return ResponseEntity.status(e.httpStatus).body(e.message)
     }
+
     @ExceptionHandler
-    fun handlerException(e:MethodArgumentNotValidException):ResponseEntity<String>{
+    fun handlerException(e: MethodArgumentNotValidException): ResponseEntity<String> {
         log.info(e.message, e)
 
         val fieldError = e.bindingResult.fieldErrors[0]
@@ -24,11 +26,11 @@ class AdminApiControllerAdvice {
 
         return ResponseEntity.badRequest().body(message)
     }
+
     @ExceptionHandler
-    fun handlerException(e:Exception):ResponseEntity<String>{
+    fun handlerException(e: Exception): ResponseEntity<String> {
         log.info(e.message, e)
 
         return ResponseEntity.internalServerError().body("시스템 오류가 발생했습니다")
     }
-
 }
